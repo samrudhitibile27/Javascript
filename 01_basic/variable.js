83e3813ee7557e08c20530dcf8f36c205a276046
samrudhitibile27 <samrudhitibile@gmail.com>
2025-08-16 11:23:55 +0000

    variable

---
+++ 01_basic/variable.js
@@ -0,0 +1,6 @@
+   const accuntId = 12345
+   let accuntEmail = "samtibile@gmail.com"
+   var accuntPasword ="54267"
+   accuntCity= "kolhapur"
+
+   console.table([accuntId,accuntEmail,accuntPasword,accuntCity])
