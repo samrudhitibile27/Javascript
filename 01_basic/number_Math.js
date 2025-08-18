5aa598ee8cf1fe9efb0a8f1193c0cf68738b5c41
samrudhitibile27 <samrudhitibile@gmail.com>
2025-08-18 11:02:32 +0000

    math_number

---
+++ 01_basic/number_Math.js
@@ -0,0 +1,25 @@
+const score = 400
+ console.log(score)
+
+ const balance = new Number(100)
+ console.log(balance)
+
+ console.log(balance.toString().length)
+ console.log(balance.toFixed(1))
+
+ const otherNumber = 123.5436
+ console.log(otherNumber.toPrecision(3))
+
+ const hundreds= 1000000
+ console.log(hundreds.toLocaleString("en-IN"))
+
+ /* * ******************** Maths ***************** */
+
+ console.log(Math)
+ console.log(Math.abs(5))
+ console.log(Math.round(5.7))
+ console.log(Math.ceil(5.7))
+ console.log(Math.max(2,3,8,4))
+ console.log(Math.min(2,3,8,4))
+ console.log(Math.random())
+ 
