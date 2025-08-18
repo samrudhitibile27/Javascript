02b4cefd20db2371b51ea31a57327030a4c6f466
samrudhitibile27 <samrudhitibile@gmail.com>
2025-08-18 07:16:57 +0000

    conversion

---
+++ 01_basic/conversion.js
@@ -0,0 +1,59 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//33 => 33
+//"33ab "=> NaN
+// true => 1,false =>0
+
+let logout ="sam"
+
+let booleanlogout = Boolean(logout)
+console.log(booleanlogout);
+
+//1 => true; 0=>false
+//"" => false
+//"sam" => true
+
+
+let stringno =33
+ let stringNumber = String(stringno)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
+
+ // ******************************** Opreations *********************************
+
+ let value = 4
+ let negValue = -value
+ console.log(negValue)
+
+ console.log(3+3);
+ console.log(3-3);
+ console.log(3*3);
+ console.log(3/3);
+ console.log(3**3);
+ console.log(3%3);
+
+ let str1= "hello"
+ let str2 = " sam"
+
+ let str3 = str1+str2
+ console.log(str3)
+
+console.log("1"+ 2)
+console.log(1+"2")
+console.log("1"+1+2)
+console.log(1+3+"4")
+
+console.log(3 + 4 - 6 * 6 % 3)
+
+let num1, num2,num3
+
+ let gamecounter=100
+ ++gamecounter
+ console.log(gamecounter)
