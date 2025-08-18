d8677fdee08ac3ab2ef82fe87f20613b780cc87a
samrudhitibile27 <samrudhitibile@gmail.com>
2025-08-18 06:00:43 +0000

    Datatypes

---
+++ 01_basic/datatype.js
@@ -0,0 +1,27 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3) // we can use node js ,not browser 
+
+console.log(3+3)
+console.log("hello")
+
+let name ="samrusdhi"//sring
+let age = 21 //number 
+let loginIn = false //
+
+//number => 2 to power 52
+ //bigint => for large number 
+ //string => ""
+ //booolean =>true or false
+ //null => standlone value , empty value
+ //undefine => variable declare but not assign value
+ //synbol => unique
+
+ /*primetive datatype*/
+
+//object 
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
