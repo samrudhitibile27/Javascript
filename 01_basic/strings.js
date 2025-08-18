aa9483aa3617b872e1b2e6354a26d220eb6f95da
samrudhitibile27 <samrudhitibile@gmail.com>
2025-08-18 10:36:14 +0000

    string

---
+++ 01_basic/strings.js
@@ -0,0 +1,24 @@
+const name ="smrudhi"
+const city ="kolhpur"
+
+console.log(`My Name Is ${name} and I am From ${city}`);
+
+const gamename = new String("candycrash")
+console.log(gamename[0])
+console.log(gamename.__proto__);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(4));
+console.log(gamename.indexOf('s'))
+console.log(gamename.indexOf("c"))
+console.log(gamename.substring(0,5))
+console.log(gamename.slice(-5,3))
+
+const name1 = "      sam     "
+console.log(name1)
+console.log(name1.trim())
+
+const url = "https://samrudhi.com/sam82tibile"
+console.log(url.replace('82','-'))
+
+console.log(url.includes("samrudhi"))
+console.log(url.split("/"))
