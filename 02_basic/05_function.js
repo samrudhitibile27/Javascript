1edc5c2938786a734cf64d0178bf69e3ec962154
samrudhitibile27 <samrudhitibile@gmail.com>
2025-08-19 12:04:23 +0000

    scope

--- 02_basic/05_function.js
+++ 02_basic/05_function.js
@@ -35,3 +35,30 @@ function lginUserMessage(username){
    return `${username} just logged in`
 }
 console.log(lginUserMessage("samrudhi"))
+
+
+
+function calculateCardPrice(...num1){
+   return num1
+}
+
+console.log(calculateCardPrice(200,300,400,4900))
+
+const user ={
+   username:"samrudhi",
+   price:200
+}
+
+function handleObjct(anyObject){
+   console.log(`UserName is ${anyObject.username} and price ${anyObject.price}`)
+}
+
+handleObjct(user)
+
+const myNewArray =[200,300,400,500]
+
+function returnObject (getArray){
+   return getArray[2]
+}
+
+console.log(returnObject(myNewArray))
---
+++ 02_basic/06_scope.js
@@ -0,0 +1,10 @@
+var c= 300
+let a= 400
+
+if (true){
+    let a = 10
+    const b =20 
+    console.log("INNER",a)
+}
+
+console.log(a);
