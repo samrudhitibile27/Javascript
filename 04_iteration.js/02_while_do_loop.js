7335e5a087ef4aa121d5f57f6713ba2f1266f86b
samrudhitibile27 <samrudhitibile@gmail.com>
2025-08-20 10:11:46 +0000

    whie do looo

---
+++ 04_iteration.js/02_while_do_loop.js
@@ -0,0 +1,22 @@
+let i = 0
+while (i<=10) {
+    console.log(`value of i is ${i}`);
+    i = i + 2
+    
+}
+
+let myarr =["kolhapur","pune","satara","sangli"]
+
+let arr = 0
+while (arr<myarr.length) {
+    console.log(`citys are ${myarr[arr]}`)
+    arr = arr +1
+    
+}
+
+
+let score = 1
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score<=5);
