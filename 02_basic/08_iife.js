7e30e51d24bc3da85e61864eca6843252a97bf4a
samrudhitibile27 <samrudhitibile@gmail.com>
2025-08-20 07:02:20 +0000

    iife

---
+++ 02_basic/08_iife.js
@@ -0,0 +1,12 @@
+(function chai(){
+    console.log(`db connected`)
+})();
+
+(()=> {
+    console.log(`db connected two`)
+})();
+
+((name)=> {
+    console.log(`db connected two,${name}`)
+})("samrudhi");
+
